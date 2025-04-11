831a0bc62c6deb22aa546a3280639ed6c8772405
Ifeanyi Ojukwu <d00365009@utahtech.edu>
2025-04-11 16:16:38 -0600

    use strict

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,8 @@
+'use strict';// helps for debugging 
+
+let HasDriversLicense =false;
+
+const passTest = true;
+
+ if(passTest) HasDriversLicense = true;
+ if(HasDriversLicense) console.log('I can drive well :D');
