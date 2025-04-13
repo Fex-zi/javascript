d5194c00a36caf3a5646516d54bbf3a385529a4a
Fexzi <fexzi87@gmail.com>
2025-04-13 14:54:19 -0600

    double functions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,6 +1,6 @@
 'use strict';// helps for debugging 
 
-let HasDriversLicense =false;
+let HasDriversLicense = false;
 
 const passTest = true;
 
@@ -15,16 +15,16 @@ const passTest = true;
  // calling / running/ invoking the function
  logger();
 
- function fruitProcessor(apples, oranges){
+ function fruitProcessor1(apples, oranges){
    // console.log(apples, oranges);
     const juice =`Juice with ${apples} apples and ${oranges} oranges.`
     return juice;
  }
  
- const appleJuice = fruitProcessor(5, 0);
+ const appleJuice = fruitProcessor1(5, 0);
  console.log(appleJuice);
 
-const appleOrangeJuice = fruitProcessor(2,4);
+const appleOrangeJuice = fruitProcessor1(2,4);
 console.log(appleOrangeJuice);
 
 //function declarations
@@ -41,3 +41,34 @@ const calcAge2 = function(birthyear){
 
 const age2 =  calcAge1(1991);
 console.log(age1, age2);
+
+//arrow function
+const calcAge3 = birthyear => 2037 - birthyear;
+const age3 = calcAge3(1991);
+console.log(age3);
+
+
+const yearUntilRetirement = (birthyear, firstname) =>{
+const Age = 2025 - birthyear;
+const retire = 65 - Age;
+return `${firstname} retires in ${retire} years`;
+}
+
+console.log(yearUntilRetirement(1993, 'Ifeanyi'));
+console.log(yearUntilRetirement(1992, 'Paige'));
+
+function cutfruits(fruits){
+
+    return fruits * 4;
+}
+
+function fruitProcessor(apples, oranges){
+    const cutApple = cutfruits(apples);
+    const cutOranges = cutfruits(oranges);
+     const juice =`Juice with ${cutApple} pieces of apples and ${cutOranges} pieces of oranges.`
+     return juice;
+  }
+  
+  //const fruitCuter = fruitProcessor(2, 5); //8, 20
+
+  console.log(fruitProcessor(2, 5));
