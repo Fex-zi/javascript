b0f2a4c2a1ffb0fd5d2f5c5b5b63a9f5fb070cf2
Ifeanyi Ojukwu <d00365009@utahtech.edu>
2025-04-30 12:40:30 -0600

    functions

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -23,7 +23,7 @@ createBooking('LH123', 2);
 createBooking('LH123', 5);
 
 console.log(bookings);
-*/
+
 
 const flight = 'LH234';
 const ifeanyi = {
@@ -54,3 +54,72 @@ const newPassport = function (person) {
 
 newPassport(ifeanyi);
 checkIn(flight, ifeanyi);
+
+const oneWord = function (str) {
+  return str.replace(/ /g, '').toLowerCase();
+};
+
+const upperFirstword = function (str) {
+  const [first, ...others] = str.split(' ');
+  return [first.toUpperCase(), ...others].join(' ');
+};
+const transformer = function (str, fn) {
+  console.log(`Original string: ${str}`);
+  console.log(`Transformed string: ${fn(str)}`);
+
+  console.log(`Transformed by : ${fn.name}`);
+};
+
+transformer('Javascript is the best!', upperFirstword);
+transformer('Javascript is the best!', oneWord);
+
+const high5 = function () {
+  console.log('👋');
+};
+
+document.body.addEventListener('click', high5);
+
+['Jonas', 'Martha', 'Adam'].forEach(high5);
+
+const greet = function (greeting) {
+  return function (name) {
+    console.log(`${greeting} ${name}`);
+  };
+};
+
+const greeterHey = greet('Hey');
+
+greeterHey('Ifeanyi');
+greeterHey('Steven');
+
+greet('Hello')('Ifeanyi');
+
+const greetArr = greeting => name => console.log(`${greeting} ${name}`);
+greet('Hey')('Ifeanyi Emmnauel');
+*/
+
+const lufthansa = {
+  airline: 'Luftansa',
+  iataCode: 'LH',
+  bookings: [],
+  book(flightNum, name) {
+    console.log(
+      `${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}`
+    );
+    this.bookings.push({ flight: `${this.iataCode}${flightNum}`, name });
+  },
+};
+
+lufthansa.book(239, 'Ifeanyi Ojukwu');
+lufthansa.book(535, 'John Smith');
+console.log(lufthansa);
+
+const euroWings = {
+  name: 'Eurowings',
+  iataCode: 'EW',
+  bookings: [],
+};
+
+const book = lufthansa.book;
+
+book(23, 'Sarah Williams');
