775f29d83f39fcc3182b990e57625feb41b076cf
Fexzi <fexzi87@gmail.com>
2025-05-04 15:09:44 -0600

    map

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -86,6 +86,13 @@ const createUsernames = function (accs) {
   });
 };
 
+const calcDisplayBalance = function (movements) {
+  const balance = movements.reduce((acc, mov) => acc + mov);
+  labelBalance.textContent = `$${balance}`;
+};
+
+calcDisplayBalance(account1.movements);
+
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 const deposits = movements.filter(function (mov) {
@@ -99,6 +106,21 @@ const withdrawals = movements.filter(function (mov) {
 
 console.log(withdrawals);
 
+console.log(`--- Im here ---`);
+// accumulator is like SNOWBALL
+// const balance = movements.reduce(function (acc, cur, i, arr) {
+//   console.log(`Iteration ${i}: ${acc}`);
+//   return acc + cur;
+// }, 0); // if 100 its starts with 100
+
+const balance = movements.reduce((acc, cur) => acc + cur, 0); // if 100 its starts with 100
+console.log(balance);
+
+const max = movements.reduce((acc, mov) => {
+  if (acc > mov) return acc;
+  else return mov;
+}, movements[0]);
+console.log(max);
 ///////////////////////////////////////
 // Coding Challenge #1
 
@@ -137,6 +159,64 @@ console.log(`---Kate's Dog below---`);
 checkDogs(dogsKate);
 console.log(`---Julia's Dog below---`);
 checkDogs(dogJulia);
+
+///////////////////////////////////////
+// Coding Challenge #2
+
+/* 
+Let's go back to Julia and Kate's study about dogs. This time, they want to convert dog ages to human ages and calculate the average age of the dogs in their study.
+
+Create a function 'calcAverageHumanAge', which accepts an arrays of dog's ages ('ages'), and does the following things in order:
+
+1. Calculate the dog age in human years using the following formula: if the dog is <= 2 years old, humanAge = 2 * dogAge. If the dog is > 2 years old, humanAge = 16 + dogAge * 4.
+2. Exclude all dogs that are less than 18 human years old (which is the same as keeping dogs that are at least 18 years old)
+3. Calculate the average human age of all adult dogs (you should already know from other challenges how we calculate averages 😉)
+4. Run the function for both test datasets
+
+TEST DATA 1: [5, 2, 4, 1, 15, 8, 3]
+TEST DATA 2: [16, 6, 10, 5, 6, 1, 4]
+
+*/
+console.log(`----Dog's AGE----`);
+const dogAge = [5, 2, 4, 1, 15, 8, 3];
+// const calcAverageHumanAge = function (ages) {
+//   // 1. Calculate human ages
+//   //const humanAges = ages.map(age => (age <= 2 ? 2 * age : 16 + age * 4));
+//   const humanAges = ages.map(function (age) {
+//     return age <= 2 ? 2 * age : 16 + age * 4;
+//   });
+//   return humanAges;
+// };
+
+// console.log(calcAverageHumanAge(dogAge));
+
+// const excludeDogs = calcAverageHumanAge(dogAge);
+
+// const NewDogAge = excludeDogs.filter(function (mov) {
+//   return mov > 18;
+// });
+// console.log(NewDogAge);
+
+// const calAvg = NewDogAge.reduce((arr, acc) => arr + acc, 0) / NewDogAge.length;
+
+// console.log(calAvg);
+
+const calcAverageHumanAge = function (ages) {
+  // 1. Calculate human ages
+  //const humanAges = ages.map(age => (age <= 2 ? 2 * age : 16 + age * 4));
+  const humanAges = ages.map(function (age) {
+    return age <= 2 ? 2 * age : 16 + age * 4;
+  });
+  const NewDogAge = humanAges.filter(function (mov) {
+    return mov >= 18;
+  });
+
+  const calAvg =
+    NewDogAge.reduce((acc, cur) => acc + cur, 0) / NewDogAge.length;
+  return calAvg;
+};
+
+console.log(calcAverageHumanAge(dogAge));
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
--- README.md
+++ README.md
@@ -1,11 +1,13 @@
 # javascript
 
 Jvascript : PHP
-.split : explode() //convert text into an array
-.join : implode() // to join/add to arrays( , -)
+.split : explode() //convert string into an array/ can't for int
+.join : implode() //convert array to strings and works with intjoin/adds( , -)
 .slice : array_slice() // to remove arrays(1, -2)
+.reduce : reduce() or array_reduce// to sum up the array
 mvs.filter(function (move) {
 return move > 0;
 }); : array_filter($mvs, function($move) {
 return $move > 0;
 });
+.map: array_map() // works like foreach but returns array
